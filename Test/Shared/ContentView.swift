da71d45019dba8484ab9fa339456d80bdc875600
Chandan <radhachandan9027@gmail.com>
2022-07-12 08:14:49 +0530

    Intial Push

---
+++ Test/Shared/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by radha chilamkurthy on 12/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+        Text("one")
+        
+        Button("Press me") {
+            debugPrint("Awwwwwwwwwwww...")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Test/Shared/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Shared
+//
+//  Created by radha chilamkurthy on 12/07/22.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
